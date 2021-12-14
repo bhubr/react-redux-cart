6047fb3eeff0d2bda163a6b90d6ba6bf0fc4045a
Benoît Hubert <benoithubert@gmail.com>
2021-12-14 19:13:59 +0100

    Add to cart button

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
 # Demo React Redux - Panier d'achat
 
 Fake API : <https://my-json-server.typicode.com/bhubr/products-api/products>
+
+
+## Structure du panier
+
+Plus simple 
+```json
+[
+  1, 3
+]
+```
+
+Plus complexe 
+
+```json
+[
+  {
+    "productId": 1,
+    "quantity": 1
+  },
+  {
+    "productId": 3,
+    "quantity": 2
+  }
+]
+```
--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,30 @@ import axios from 'axios'
 import logo from './logo.svg'
 import './App.css'
 
+/*
+{
+    "id": 1,
+    "title": "Sony KE55XH90P",
+    "slug": "sony-ke55xh90p",
+    "description": "Sony TV 55 Pouces - Bluetooth, Wi-fi, USB, Ethernet, HDMI",
+    "picture": "https://m.media-amazon.com/images/I/81+sKrRZa+L._AC_SL1500_.jpg",
+    "price": 999,
+    "onSale": false
+  }
+*/
+
+
 function App() {
-  const [products, setProducts] = useState(null)
+  const [products, setProducts] = useState([])
+  const [cart, setCart] = useState([])
+
+  const addToCart = (id) => {
+    if (!cart.includes(id)) {
+      // setCart(cart.concat(id))
+      // setCart([...cart, id])
+      setCart(prevCart => [...prevCart, id])
+    } 
+  }
 
   useEffect(() => {
 axios.get('https://my-json-server.typicode.com/bhubr/products-api/products').then(function (response) {
@@ -13,7 +35,21 @@ axios.get('https://my-json-server.typicode.com/bhubr/products-api/products').the
   }, [])
   return (
     <div className="App">
-      
+      {
+        products.map(({title, id, price}) => {
+          return <div key={id}>
+            <h1>{title}</h1>
+            <p>{price}</p>
+            <button type="button" onClick={() => addToCart(id)}>Ajouter au panier</button>
+          </div>
+        })
+      }
+      <h2>Cart</h2>
+      {
+        cart.map(id => (
+          <p key={id}>{id}</p>
+        ))
+      }
     </div>
   )
 }
